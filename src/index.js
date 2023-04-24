c61b83806fcbd2688605f81b93849562de5eed7f
pan <panw3i@gmail.com>
2023-04-24 21:58:48 +0800

    Add initial project files with default ignored files, module configuration, and basic scene setup with cube and OrbitControls in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { AxesHelper } from 'three';
+const axesHelper = new AxesHelper(5);
 
 // Create a scene
 const scene = new THREE.Scene();
@@ -16,16 +19,21 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // Add a cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+scene.add(axesHelper);
 
 // Render loop
 function animate() {
   requestAnimationFrame(animate);
+
+  // Update OrbitControls
+  controls.update();
 
   // Rotate the cube
   cube.rotation.x += 0.01;
