40b8c4931b24bf1c85af818e3690d12ed973dab0
pan <panw3i@gmail.com>
2023-04-24 22:34:27 +0800

    Add GSAP and Parcel as dependencies, and create a rotating animation for the cube using GSAP

--- src/index.js
+++ src/index.js
@@ -1,49 +1,61 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { AxesHelper } from 'three';
-const axesHelper = new AxesHelper(5);
-const clock = new THREE.Clock();
+import * as THREE from 'three';
+import { gsap } from 'gsap';
 
-// Create a scene
+// 创建场景
 const scene = new THREE.Scene();
 
-// Add a camera
+// 创建相机
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
 camera.position.z = 5;
 
-// Create a renderer
+// 创建渲染器
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-const controls = new OrbitControls(camera, renderer.domElement);
 
-// Add a cube
+// 创建立方体
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-scene.add(axesHelper);
 
-// Render loop
+
+let isAnimationPlaying = false; // 是否正在播放动画
+// 为单击事件添加事件监听器
+window.addEventListener('click', () => {
+  if (isAnimationPlaying) {
+    // 停止动画
+    timeline.pause();
+  } else {
+    // 开始动画
+    timeline.play();
+  }
+  isAnimationPlaying = !isAnimationPlaying;
+});
+const timeline = gsap.timeline({ paused: true }); // 创建一个暂停的时间轴
+
+// 使用GSAP创建立方体旋转动画
+const initialRotationY = cube.rotation.y;
+timeline.to(cube.rotation, {
+  duration: 2,
+  y: initialRotationY + Math.PI * 2,
+  ease: 'line',
+  yoyo: true, // 循环
+  repeat: -1, // 无限循环
+});
+
+// 动画循环
 function animate() {
-  // 在下一帧时调用animate函数
   requestAnimationFrame(animate);
 
-  // 获取时间差
-  const deltaTime = clock.getDelta();
-
-  // 使用时间差来移动立方体
-  cube.position.x += 1 * deltaTime;
-
-  // 更新控制器和渲染场景
-  controls.update();
+  // 渲染场景
   renderer.render(scene, camera);
 }
 
-
+// 启动动画循环
 animate();
